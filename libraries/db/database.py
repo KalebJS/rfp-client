5c1a8c2d1cd041d97564e66b26b779ef9422675f
Kaleb Smith <kalebsmith11@gmail.com>
2023-10-31 12:25:31 -0600

    feature: implement tables in db

--- libraries/db/database.py
+++ libraries/db/database.py
@@ -1,6 +1,9 @@
 from pathlib import Path
-from sqlmodel import SQLModel, Session, create_engine
-from .model import Answer
+from uuid import uuid4
+
+from sqlmodel import Session, SQLModel, create_engine
+
+from .model import APIToken, Answer, Question
 
 
 class DatabaseProxy:
@@ -12,6 +15,7 @@ class DatabaseProxy:
         database_file_path.parent.mkdir(parents=True, exist_ok=True)
         sqlite_url = f"sqlite:///{database_file_path}"
         engine = create_engine(sqlite_url)
+        SQLModel.metadata.drop_all(engine)
         SQLModel.metadata.create_all(engine)
         return engine
 
@@ -20,3 +24,41 @@ class DatabaseProxy:
             answer = session.get(Answer, id)
             return answer.text
 
+    def insert_answer(self, answer: Answer):
+        with Session(self.engine) as session:
+            session.add(answer)
+            session.commit()
+            session.refresh(answer)
+            return answer
+        
+    def insert_question(self, question: Question):
+        with Session(self.engine) as session:
+            session.add(question)
+            session.commit()
+            session.refresh(question)
+            return question
+    
+    def create_api_token(self):
+        token = uuid4().hex
+        token_hash = hash(token)
+        with Session(self.engine) as session:
+            api_token = APIToken(token_hash=token_hash)
+            session.add(api_token)
+            session.commit()
+            session.refresh(api_token)
+            return token
+
+    def validate_token(self, token: str) -> bool:
+        with Session(self.engine) as session:
+            token_hash = hash(token)
+            api_token = session.get(APIToken, token_hash)
+            return api_token is not None
+
+    def get_questions(self):
+        with Session(self.engine) as session:
+            return session.query(Question).all()
+        
+    def get_answers(self):
+        with Session(self.engine) as session:
+            return session.query(Answer).all()
+    
--- libraries/db/model.py
+++ libraries/db/model.py
@@ -1,8 +1,20 @@
 from typing import Optional
-from sqlmodel import SQLModel, Field
+
+from sqlmodel import Field, SQLModel
 
 
 class Answer(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     text: str
+    linked_org: Optional[str] = None
 
+
+class Question(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    text: str
+    answer_id: Optional[int] = Field(default=None, foreign_key="answer.id")
+
+
+class APIToken(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    token_hash: str
